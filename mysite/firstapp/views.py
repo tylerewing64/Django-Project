0d99c18c0898ae4fba937650f970af98fa9fe7a9
tylerewing64 <76537101+tylerewing64@users.noreply.github.com>
2022-06-14 22:42:40 -0400

    Template created

--- mysite/firstapp/views.py
+++ mysite/firstapp/views.py
@@ -1,3 +1,7 @@
+from email.policy import HTTP
+from django.http import HttpResponse
 from django.shortcuts import render
-
 # Create your views here.
+
+def home(request):
+    return render (request, 'home.html',)
